aa3dacd7d6b6a23e7f9fa20ee05bf8c9dc1a0dc4
hutiwephy <68164156+hutiwephy@users.noreply.github.com>
2025-05-31 14:32:59 +0100

    Removed Build scripts

--- build.bat
+++
@@ -1,2 +0,0 @@
-cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=MinSizeRel -B./cmake-config -G "Ninja"
-cmake --build ./cmake-config --config MinSizeRel
